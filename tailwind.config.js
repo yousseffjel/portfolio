d2f73ecc40a7a006b09c7bfc5f2eae63ea9cdb58
Youssef Fjel <youssef.fjel-etu@etu.univh2c.ma>
2024-11-04 20:15:54 +0100

    update the tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,12 @@
 module.exports = {
   purge: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
-  darkMode: 'class', // Enable dark mode
+  darkMode: 'class', // Enable dark modetm
   theme: {
     extend: {
       keyframes: {
         fadeIn: {
-          '0%': { opacity: 0 },
-          '100%': { opacity: 1 },
+          '0%': { opacity: 0, transform: 'translateY(10px)' },
+          '100%': { opacity: 1, transform: 'translateY(0)' },
         },
         slideInLeft: {
           '0%': { transform: 'translateX(-100%)' },
@@ -14,7 +14,7 @@ module.exports = {
         },
       },
       animation: {
-        fadeIn: 'fadeIn 1s ease-in-out',
+        fadeIn: 'fadeIn 0.8s ease-out forwards',
         slideInLeft: 'slideInLeft 1s ease-out',
       },
     },
